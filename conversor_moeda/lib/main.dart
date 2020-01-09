7c6ca754f7962cde718fdb72a71eecee3c4718c9
Adriano Ament <adrianojament@gmail.com>
2020-01-08 22:17:30 -0300

    Novas Alteracoes

---
+++ conversor_moeda/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?key=7ab07514";
+
+void main() async {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  "Erro ao carregar dados",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+
+            return Center(
+              child: Container(color: Colors.green),
+            );
+          },
+        ));
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
